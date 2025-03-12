794f6e826222f5ca2c06d989adf5d4206267747f
Mamdouh Alsaudi <159956773+MamdouhAlsaodi@users.noreply.github.com>
2025-03-12 06:27:51 -0300

    BANK.cpp

--- BANK/BANK.cpp
+++ BANK/BANK.cpp
@@ -4,9 +4,7 @@
 #include "clsUtil.h"
 int main()
 {
-    //cout <<clsUtil::EncryptText("M9695") << endl;
-    //cout <<clsUtil::EncryptText("M102") << endl;
-    //cout <<clsUtil::EncryptText("M1911") << endl;
+
     while(true)
     {
         if (!clsLoginScreen::ShowLoginScreen())
